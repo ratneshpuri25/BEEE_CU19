7416b3bdc4d8f879bb32273a7a602bdfae550f27
ratneshpuri25 <55545571+ratneshpuri25@users.noreply.github.com>
2019-09-19 22:28:34 +0530

    Update Code_file.ino

--- Exp2_LED_Chaser/Code_file.ino
+++ Exp2_LED_Chaser/Code_file.ino
@@ -1,1 +1,20 @@
+int i;
+void setup()
+{
 
+  for(i=10;i<=13;i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop()
+{
+for(i=10;i<=13;i++)
+{
+  digitalWrite(i,HIGH);
+  delay(500);
+  digitalWrite(i,LOW);
+  delay(500);
+}
+}
